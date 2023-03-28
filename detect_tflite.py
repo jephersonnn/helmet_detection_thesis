a2f0a43e2ef0427d369cb4ead9924b72fe8331e6
Jeph <ericjeffrey.sch@gmail.com>
2023-03-28 18:01:03 +0800

    ready for RPi, add helmet detect timeout and fps

--- detect_tflite.py
+++ detect_tflite.py
@@ -8,9 +8,14 @@ import numpy as np
 import cv2
 import time
 
+# -----please uncomment on RPi-------
+# from tensorflow.keras.preprocessing.image import img_to_array
+# from tflite_runtime.interpreter import Interpreter
+
 model_path = '//Users/jeph/Dev/Python/Helmet_Detection/Models/new model9.tflite'
-#model_path = '//Users/jeph/Dev/Python/Helmet_Detection/Models/good trial_model Mar-10-2023 13_35_47.tflite'
+# model_path = '//Users/jeph/Dev/Python/Helmet_Detection/Models/good trial_model Mar-10-2023 13_35_47.tflite'
 interpreter = tf.lite.Interpreter(model_path=model_path)
+#interpreter = Interpreter(model_path=model_path)
 
 cap = cv2.VideoCapture(1)
 neutral_time = time.time()
@@ -28,9 +33,8 @@ while cap.isOpened():
     input_data = tf.keras.utils.img_to_array(input_frame)
     input_data = tf.expand_dims(input_data, 0)
 
-
     # Run inference on the TFLite model.
-    detect =  interpreter.get_signature_runner('serving_default')
+    detect = interpreter.get_signature_runner('serving_default')
 
     # Postprocess the output.
     class_names = ["helmet-off", "helmet-on"]
@@ -39,18 +43,20 @@ while cap.isOpened():
     status = class_names[np.argmax(score)]
     confidence = 100 * np.max(score)
 
-    color = 255,255,255 #TODO modify and apply threshold
+    color = 255, 255, 255  # TODO modify and apply threshold
     if status == "helmet-on":
-        color = 0,255,0
-        neutral_time = time.time() #reset timeout
-    else: color = 0,0,255
+        color = 0, 255, 0
+        neutral_time = time.time()  # reset timeout when helmet is on
+    else:
+        color = 0, 0, 255
 
+    #If n seconds has elapsed while helmet is off
     if elapsed_time >= helmet_off_timeout:
         cv2.putText(frame, "Please wear a helmet", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
     # Display the frame with the predicted class label.
     cv2.putText(frame, status + " " + str(confidence), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-    cv2.putText(frame, str(fps) + "fps", (700,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+    cv2.putText(frame, str(fps) + "fps", (700, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
 
     cv2.imshow("Frame", frame)
 
